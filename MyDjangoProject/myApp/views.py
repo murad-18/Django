fe97ad40648374d7795852f52f371c5ff1b243e1
murad-18 <tikkamasala286@gmail.com>
2023-11-18 20:00:35 +0500

    Chat GPT integrated

--- MyDjangoProject/myApp/views.py
+++ MyDjangoProject/myApp/views.py
@@ -1,3 +1,4 @@
+from openai import OpenAI
 from django.shortcuts import render
 from .models import Person
 # Create your views here.
@@ -17,12 +18,29 @@ def contactus(request):
     return render(request, "contact.html")
 
 
+def gpt_process(strVal):
+
+    client = OpenAI(
+        api_key="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")
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are an assistant, you have to give answer correctly and give your answer in a single line minimum."},
+            {"role": "user", "content": strVal}
+        ]
+    )
+    return str(completion.choices[0].message.content)
+
+
 def generateNum(request):
     result = None
+    gptProcessed = None
     if request.method == "POST":
-        mynum = int(request.POST['number'])
-        result = mynum*125
-        myObj = Person(userinputvalue=mynum, mycalvalue=result)
+        myInput = str(request.POST['text'])
+        gptProcessed = gpt_process(myInput)
+        result = gptProcessed
+        myObj = Person(userinputvalue=myInput, mycalvalue=gptProcessed)
         myObj.save()
-        print(mynum)
+        # print(mynum)
     return render(request, "generatenum.html", {'result': result})
